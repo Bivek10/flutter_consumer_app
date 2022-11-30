db07768dfada42437ac4861e5aea25fcea77241d
Bivek10 <bibekkarki662@gmail.com>
2022-11-30 10:24:47 +0545

    add the maker line

--- lib/src/pages/view_order/order_tracking.dart
+++ lib/src/pages/view_order/order_tracking.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+
 import 'package:flutter_skeleton/src/widgets/molecules/header.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import '../../core/utils/app_secrets.skeleton.dart';
 
 class OrderTrackingMap extends StatefulWidget {
   final Map<String, dynamic> loc;
@@ -14,6 +17,11 @@ class OrderTrackingMap extends StatefulWidget {
 
 class _OrderTrackingMapState extends State<OrderTrackingMap> {
   Set<Marker> markers = {};
+
+// make sure to initialize before map loading
+
+  Set<Polyline> _polyline = {};
+  List<LatLng> polylineCoordinates = [];
   LatLng startLocation = LatLng(
     double.parse("27.708869977980832"),
     double.parse("85.32769817858934"),
@@ -22,10 +30,11 @@ class _OrderTrackingMapState extends State<OrderTrackingMap> {
     double.parse("27.710372210171386"),
     double.parse("85.3283955529332"),
   );
+  List<LatLng> latLen = [];
 
   //27.710372210171386, 85.3283955529332
 
-  setMaker() {
+  /*
     markers.addAll({
       Marker(
         //add start location marker
@@ -44,7 +53,7 @@ class _OrderTrackingMapState extends State<OrderTrackingMap> {
         ),
       ),
     });
-  }
+    */
 
   @override
   void initState() {
@@ -66,13 +75,55 @@ class _OrderTrackingMapState extends State<OrderTrackingMap> {
           myLocationEnabled: true,
           myLocationButtonEnabled: false,
           buildingsEnabled: false,
+          compassEnabled: false,
+          polylines: _polyline,
           onMapCreated: (GoogleMapController controller) {},
           markers: markers,
           initialCameraPosition: CameraPosition(
             target: startLocation,
-            zoom: 15,
+            zoom: 16,
           ),
           onTap: (LatLng) {}),
     );
+  }
+
+  setMaker() async {
+    latLen = [startLocation, endLocation];
+
+    markers.addAll(
+      // added markers
+      {
+        Marker(
+          markerId: MarkerId(latLen[0].toString()),
+          position: latLen[0],
+          infoWindow: const InfoWindow(
+            title: 'Delivery Boy',
+            snippet: 'Order On the way',
+          ),
+          icon: BitmapDescriptor.defaultMarker,
+        ),
+        Marker(
+          markerId: MarkerId(latLen[1].toString()),
+          position: latLen[1],
+          infoWindow: const InfoWindow(
+            title: 'Destination',
+            snippet: 'User location',
+          ),
+          icon: BitmapDescriptor.defaultMarker,
+        ),
+      },
+    );
+    setState(() {});
+
+    for (int i = 0; i < latLen.length; i++) {
+      _polyline.add(
+        Polyline(
+          polylineId: const PolylineId('1'),
+          points: latLen,
+          width: 5,
+          color: Colors.green,
+        ),
+      );
+    }
   }
 }
